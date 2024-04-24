50611dcaab8992b79b6774af4a6b521a1760364f
rprabhakar045 <rprabhakar045@gmail.com>
2024-04-24 12:19:13 +0530

    Initial commit

---
+++ src/ex16042024/Lab001.java
@@ -0,0 +1,11 @@
+package ex16042024;
+
+public class Lab001 {
+    public static void main(String[] Roshan) {
+        System.out.println("Roshan");
+int age= 30;
+age= age + 5;
+System.out.println(age);
+
+    }
+}
